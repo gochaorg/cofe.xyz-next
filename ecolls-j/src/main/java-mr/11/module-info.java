72a28def7ce0e1ea198cb837404213374699be13
Kamnev Georgiy <nt.gocha@gmail.com>
2020-04-09 02:55:57 +0500

    added missing exports

--- ecolls-j/src/main/java-mr/11/module-info.java
+++ ecolls-j/src/main/java-mr/11/module-info.java
@@ -4,10 +4,13 @@
 open module xyz.cofe.ecolls {
     requires java.base;
     requires transitive java.logging;
+
     exports xyz.cofe.ecolls;
     exports xyz.cofe.collection.graph;
     exports xyz.cofe.collection;
+    exports xyz.cofe.fn;
     exports xyz.cofe.iter;
     exports xyz.cofe.num;
     exports xyz.cofe.scn;
+    exports xyz.cofe.sort;
 }
