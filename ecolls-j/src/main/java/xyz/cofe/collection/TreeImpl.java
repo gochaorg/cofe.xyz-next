eaa4617b77b340c8ba13c4e2d4be440ccad784b1
Kamnev Georgiy <nt.gocha@gmail.com>
2020-07-01 17:10:27 +0500

    java doc

--- ecolls-j/src/main/java/xyz/cofe/collection/TreeImpl.java
+++ ecolls-j/src/main/java/xyz/cofe/collection/TreeImpl.java
@@ -15,24 +15,52 @@ public class TreeImpl {
         return lst;
     }
 
+    /**
+     * Получение кол-ва дочерних узлов
+     * @param tree родительский узел
+     * @return кол-во узлов
+     */
     public static int nodesCount(Tree tree) {
         if( tree == null ) throw new IllegalArgumentException("tree == null");
         return nodesOf(tree).size();
     }
 
+    /**
+     * Получение дочернего элемента по индексу
+     * @param tree родительский узел
+     * @param index индекс
+     * @param <A> Тип узла
+     * @return Дочерний узел
+     */
     public static <A extends Tree<A>> A node(Tree<A> tree, int index) {
         if( tree == null ) throw new IllegalArgumentException("tree == null");
         List<A> lst = nodesOf(tree);
         return nodesOf(tree).get(index);
     }
 
+    /**
+     * Замена дочернего узла
+     * @param tree родительский узел
+     * @param index идекс узлов которые необходимо заменить
+     * @param values дочерние узлы
+     * @param <A> Тип узла
+     * @return  Список изменений (index, oldChildNode, newChildNode)
+     */
     public static <A extends Tree<A>> List<Triple<Integer,A,A>> set(Tree<A> tree, int index, A ... values){
         List<Triple<Integer,A,A>> addList = new ArrayList<>();
         set(tree,index,values,addList::add);
         return addList;
     }
 
-    public static <A extends Tree<A>> void set(Tree<A> tree, int index, A[] values, Consumer<Triple<Integer,A,A>> added){
+    /**
+     * Замена дочернего узла
+     * @param tree родительский узел
+     * @param index идекс узлов которые необходимо заменить
+     * @param values дочерние узлы
+     * @param changes Список изменений (index, oldChildNode, newChildNode)
+     * @param <A> Тип узла
+     */
+    public static <A extends Tree<A>> void set(Tree<A> tree, int index, A[] values, Consumer<Triple<Integer,A,A>> changes){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( values == null )throw new IllegalArgumentException( "values == null" );
 
@@ -45,7 +73,7 @@ public class TreeImpl {
 
             if( idx>=0 && idx<nodes.size() ){
                 A old = nodes.set(idx,val);
-                if( added!=null )added.accept(Triple.of(idx,old,val));
+                if( changes!=null )changes.accept(Triple.of(idx,old,val));
             }else {
                 throw new IllegalArgumentException("index (="+idx+") out of bound");
             }
@@ -53,13 +81,29 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Замена дочернего узла
+     * @param tree родительский узел
+     * @param index идекс узлов которые необходимо заменить
+     * @param values дочерние узлы
+     * @param <A> Тип узла
+     * @return  Список изменений (index, oldChildNode, newChildNode)
+     */
     public static <A extends Tree<A>> List<Triple<Integer,A,A>> set(Tree<A> tree, int index, Iterable<A> values){
         List<Triple<Integer,A,A>> addList = new ArrayList<>();
         set(tree,index,values,addList::add);
         return addList;
     }
 
-    public static <A extends Tree<A>> void set(Tree<A> tree, int index, Iterable<A> values, Consumer<Triple<Integer,A,A>> added){
+    /**
+     * Замена дочернего узла
+     * @param tree родительский узел
+     * @param index идекс узлов которые необходимо заменить
+     * @param values дочерние узлы
+     * @param changes Список изменений (index, oldChildNode, newChildNode)
+     * @param <A> Тип узла
+     */
+    public static <A extends Tree<A>> void set(Tree<A> tree, int index, Iterable<A> values, Consumer<Triple<Integer,A,A>> changes){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( values == null )throw new IllegalArgumentException( "values == null" );
 
@@ -72,7 +116,7 @@ public class TreeImpl {
 
             if( idx>=0 && idx<nodes.size() ){
                 A old = nodes.set(idx,val);
-                if( added!=null )added.accept(Triple.of(idx,old,val));
+                if( changes!=null )changes.accept(Triple.of(idx,old,val));
             }else {
                 throw new IllegalArgumentException("index (="+idx+") out of bound");
             }
@@ -80,12 +124,28 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Добавление дочернего узла в указанную позицию списка дочерних улов
+     * @param tree родительский узел
+     * @param index индекс (0..) позиции вставки
+     * @param nodes дочерние узлы
+     * @param <A> Тип узла
+     * @return список добавленных узлов - fn( index, null, insertedChildNode )
+     */
     public static <A extends Tree<A>> List<Triple<Integer,A,A>> insert(Tree<A> tree, int index, A ... nodes){
         List<Triple<Integer,A,A>> addList = new ArrayList<>();
         insert(tree,index,nodes, addList::add);
         return addList;
     }
 
+    /**
+     * Добавление дочернего узла в указанную позицию списка дочерних улов
+     * @param tree родительский узел
+     * @param index индекс (0..) позиции вставки
+     * @param nodes дочерние узлы
+     * @param added список добавленных узлов - fn( index, null, insertedChildNode )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void insert(Tree<A> tree, int index, A[] nodes, Consumer<Triple<Integer,A,A>> added){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( nodes == null )throw new IllegalArgumentException( "nodes == null" );
@@ -102,12 +162,27 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Добавление дочернего узла в указанную позицию списка дочерних улов
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param <A> Тип узла
+     * @return  список добавленных узлов - fn( index, null, insertedChildNode )
+     */
     public static <A extends Tree<A>> List<Triple<Integer,A,A>> insert(Tree<A> tree, int index, Iterable<A> nodes){
         List<Triple<Integer,A,A>> addList = new ArrayList<>();
         insert(tree,index,nodes,e -> addList.add(e));
         return addList;
     }
 
+    /**
+     * Добавление дочернего узла в указанную позицию списка дочерних улов
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param index индекс (0..) позиции вставки
+     * @param added список добавленных узлов - fn( index, null, insertedChildNode )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void insert(Tree<A> tree, int index, Iterable<A> nodes, Consumer<Triple<Integer,A,A>> added){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( nodes == null )throw new IllegalArgumentException( "nodes == null" );
@@ -124,12 +199,26 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Добавление дочернего узла в конец списка
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param <A> Тип узла
+     * @return  список добавленных узлов - fn( index, null, insertedChildNode )
+     */
     public static <A extends Tree<A>> List<Triple<Integer,A,A>> append(A tree, A ... nodes){
         List<Triple<Integer,A,A>> addList = new ArrayList<>();
-        append(tree,nodes,e -> addList.add(e));
+        append(tree,nodes, addList::add);
         return addList;
     }
 
+    /**
+     * Добавление дочернего узла в конец списка
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param added список добавленных узлов - fn( index, null, insertedChildNode )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void append(A tree, A[] nodes, Consumer<Triple<Integer,A,A>> added){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( nodes == null )throw new IllegalArgumentException( "nodes == null" );
@@ -145,10 +234,23 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Добавление дочернего узла в конец списка
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void append(A tree, Iterable<A> nodes){
         append(tree,nodes,null);
     }
 
+    /**
+     * Добавление дочернего узла в конец списка
+     * @param tree родительский узел
+     * @param nodes дочерние узлы
+     * @param added список добавленных узлов - fn( index, null, insertedChildNode )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void append(A tree, Iterable<A> nodes, Consumer<Triple<Integer,A,A>> added){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( nodes == null )throw new IllegalArgumentException( "nodes == null" );
@@ -164,10 +266,23 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param index иднекс дочернего элемента(ов)
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByIndex(Tree<A> tree, int ... index){
         deleteByIndex(tree,index,null);
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param index иднекс дочернего элемента(ов)
+     * @param deleted возможно null, уведомление о удаленных элементах: fn( index, deletedChildNode, null )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByIndex(Tree<A> tree, int[] index, Consumer<Triple<Integer,A,A>> deleted){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( index == null )throw new IllegalArgumentException( "index == null" );
@@ -182,10 +297,23 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param index иднекс дочернего элемента(ов)
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByIndex(Tree<A> tree, Iterable<Integer> index){
         deleteByIndex(tree,index,null);
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param index иднекс дочернего элемента(ов)
+     * @param deleted возможно null, уведомление о удаленных элементах: fn( index, deletedChildNode, null )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByIndex(Tree<A> tree, Iterable<Integer> index, Consumer<Triple<Integer,A,A>> deleted){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( index == null )throw new IllegalArgumentException( "index == null" );
@@ -205,12 +333,24 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param value дочерние удаляемые элементы
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByValue(Tree<A> tree, A ... value){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( value == null )throw new IllegalArgumentException( "value == null" );
         nodesOf(tree).removeAll(Arrays.asList(value));
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param value дочерние удаляемые элементы
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByValue(Tree<A> tree, Iterable<A> value){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( value == null )throw new IllegalArgumentException( "value == null" );
@@ -221,12 +361,26 @@ public class TreeImpl {
         nodesOf(tree).removeAll(removeSet);
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param value дочерние удаляемые элементы
+     * @param deleted возможно null, уведомление о удаленных элементах: fn( index, deletedChildNode, null )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByValue(Tree<A> tree, A[] value, Consumer<Triple<Integer,A,A>> deleted){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( value == null )throw new IllegalArgumentException( "value == null" );
         deleteByValue(tree,Arrays.asList(value),deleted);
     }
 
+    /**
+     * Удаление дочернего узла(ов)
+     * @param tree родительский узел
+     * @param value дочерние удаляемые элементы
+     * @param deleted возможно null, уведомление о удаленных элементах: fn( index, deletedChildNode, null )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void deleteByValue(Tree<A> tree, Iterable<A> value, Consumer<Triple<Integer,A,A>> deleted){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         if( value == null )throw new IllegalArgumentException( "value == null" );
@@ -249,11 +403,22 @@ public class TreeImpl {
         }
     }
 
+    /**
+     * Удаление всех дочерних узлов
+     * @param tree родительский узел
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void clear(Tree<A> tree){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         nodesOf(tree).clear();
     }
 
+    /**
+     * Удаление всех дочерних узлов
+     * @param tree родительский узел
+     * @param deleted возможно null, уведомление о удаленных элементах: fn( index, deletedChildNode, null )
+     * @param <A> Тип узла
+     */
     public static <A extends Tree<A>> void clear(Tree<A> tree, Consumer<Triple<Integer,A,A>> deleted){
         if( tree == null )throw new IllegalArgumentException( "tree == null" );
         List<A> nodes = nodesOf(tree);
