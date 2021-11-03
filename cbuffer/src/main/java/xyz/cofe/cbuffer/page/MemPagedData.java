3fb3b5ff13355d5fc3c8651964cc50ec41f32e91
Kamnev Georgiy <nt.gocha@gmail.com>
2021-11-04 02:49:25 +0500

    extract MemInfoUsed

--- cbuffer/src/main/java/xyz/cofe/cbuffer/page/MemPagedData.java
+++ cbuffer/src/main/java/xyz/cofe/cbuffer/page/MemPagedData.java
@@ -2,7 +2,6 @@ package xyz.cofe.cbuffer.page;
 
 import xyz.cofe.fn.Tuple2;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 
 /**
@@ -40,92 +39,7 @@ public class MemPagedData implements PagedData, ExtendablePages, ReduciblePages,
         this.dataSize = 0;
     }
 
-    //region memInfo, memoryInfo(),
-    protected class MemInfoUsed implements UsedPagesInfo, Capacity {
-        @Override
-        public int pageCount() {
-            int pc = dataSize / pageSize;
-            int pc_d = dataSize % pageSize;
-            return pc_d>0 ? pc+1 : pc;
-        }
-
-        @Override
-        public int lastPageSize() {
-            return dataSize % pageSize;
-        }
-
-        @Override
-        public int pageSize() {
-            return pageSize;
-        }
-
-        @Override
-        public BigInteger capacity() {
-            return BigInteger.valueOf(buffer.length);
-        }
-
-        @SuppressWarnings("MethodDoesntCallSuperMethod")
-        @Override
-        public UsedPagesInfo clone() {
-            return new MemInfoUsedClone(this);
-        }
-    }
-    protected static class MemInfoUsedClone implements UsedPagesInfo, Capacity {
-        protected BigInteger capacity;
-        protected int pageCount;
-        protected int pageSize;
-        protected int lastPageSize;
-
-        public MemInfoUsedClone(BigInteger capacity, int pageCount, int pageSize, int lastPageSize) {
-            this.capacity = capacity;
-            this.pageCount = pageCount;
-            this.pageSize = pageSize;
-            this.lastPageSize = lastPageSize;
-        }
-
-        public MemInfoUsedClone(MemInfoUsedClone sample){
-            if( sample==null )throw new IllegalArgumentException( "sample==null" );
-            this.capacity = sample.capacity;
-            this.pageCount = sample.pageCount;
-            this.pageSize = sample.pageSize;
-            this.lastPageSize = sample.lastPageSize;
-        }
-
-        public MemInfoUsedClone(MemInfoUsed sample){
-            if( sample==null )throw new IllegalArgumentException( "sample==null" );
-            this.capacity = sample.capacity();
-            this.pageCount = sample.pageCount();
-            this.pageSize = sample.pageSize();
-            this.lastPageSize = sample.lastPageSize();
-        }
-
-        @Override
-        public BigInteger capacity() {
-            return capacity;
-        }
-
-        @Override
-        public int pageCount() {
-            return pageCount;
-        }
-
-        @Override
-        public int lastPageSize() {
-            return lastPageSize;
-        }
-
-        @Override
-        public int pageSize() {
-            return pageSize;
-        }
-
-        @SuppressWarnings("MethodDoesntCallSuperMethod")
-        @Override
-        public UsedPagesInfo clone() {
-            return new MemInfoUsedClone(this);
-        }
-    }
-    protected MemInfoUsed memInfo = new MemInfoUsed();
+    protected MemInfoUsed memInfo = new MemInfoUsed(this);
 
     @Override
     public UsedPagesInfo memoryInfo() { return memInfo; }
