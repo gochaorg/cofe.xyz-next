06e883db18f46881573aacce7f1c63dfb67b505a
Kamnev Georgiy <nt.gocha@gmail.com>
2021-11-05 05:21:33 +0500

    javadoc

--- cbuffer/src/main/java/xyz/cofe/cbuffer/page/PageLock.java
+++ cbuffer/src/main/java/xyz/cofe/cbuffer/page/PageLock.java
@@ -1,6 +1,22 @@
 package xyz.cofe.cbuffer.page;
 
+/**
+ * Блокировка диапазона страниц
+ */
 public interface PageLock {
+    /**
+     * Блокировка диапазона страниц для записи - эксклюзивная блокировка
+     * @param from начало диапазона
+     * @param toExc (исключительно) конец диапазона
+     * @param code код исполняемый во время блокировки
+     */
     void writePageLock( int from, int toExc, Runnable code );
+
+    /**
+     * Блокировка диапазона страниц для чтения - shared блокировка
+     * @param from начало диапазона
+     * @param toExc (исключительно) конец диапазона
+     * @param code код исполняемый во время блокировки
+     */
     void readPageLock( int from, int toExc, Runnable code );
 }
